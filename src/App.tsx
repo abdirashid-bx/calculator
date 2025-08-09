d1f10b878a0bcdc68860276c31d44812f71d00eb
abdirashid-bx <cabdirashidbashirxasan@gmail.com>
2025-08-09 23:05:37 +0300

    quiz app

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+ 
+import './App.css'
+ 
+import Quizapp from './component/Quizapp'
+ 
+ 
+function App() {
+  
+ 
+ 
+
+  
+  return (
+  
+<Quizapp/>
+    
+
+  )
+}
+ 
+export default App
---
+++ src/Question.js
@@ -0,0 +1,36 @@
+export const questions = [
+  {
+    question: "What is the largest city in the world by population?",
+    options: ["Tokyo", "New York City", "Mumbai"],
+    correct: "Tokyo"
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    options: ["Mars", "Jupiter", "Saturn"],
+    correct: "Mars"
+  },
+  {
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    options: ["William Shakespeare", "Charles Dickens", "Jane Austen"],
+    correct: "William Shakespeare"
+  },
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "Berlin", "Madrid"],
+    correct: "Paris"
+  },
+  {
+    question: "Which element has the chemical symbol 'O'?",
+    options: ["Oxygen", "Gold", "Iron"],
+    correct: "Oxygen"
+  },
+  {
+    question: "What is the longest river in the world?",
+    options: ["Amazon River", "Nile River", "Yangtze River"],
+    correct: "Nile River"
+  },{
+  question: "What is the largest planet in our solar system?",
+  options: ["Earth", "Jupiter", "Saturn"],
+  correct: "Jupiter"
+}
+];
